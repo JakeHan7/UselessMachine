b350f8a70b43522638fa4c16d42a14514eec4ce2
JakeHan7 <jakehan7@gmail.com>
2018-08-29 08:52:38 -0700

    Added countdown to the switch.

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.uselessmachine;
 
 import android.content.Context;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +31,41 @@ public class MainActivity extends AppCompatActivity {
         switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
+                if (isChecked) {
+                    startSwitchOffTimer();
                     Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
                 }
             }
+
+
         });
     }
+    private void startSwitchOffTimer() {
+        new CountDownTimer(5000, 100) {
+            @Override
+            public void onTick(long l) {
+                if (!switchUseless.isChecked()){
+                    Log.d(TAG, "onTick: canceling");
+                    cancel();
+                }
 
-    private void wireWidgets(){
-        buttonSelfDestruct = findViewById(R.id.button_main_selfdestruct);
-        switchUseless = findViewById(R.id.switch_main_useless);
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                Log.d(TAG, "onFinish: switch set to false");
+
+            }
+
+        }.start();
+
     }
-}
+
+
+        private void wireWidgets () {
+            buttonSelfDestruct = findViewById(R.id.button_main_selfdestruct);
+            switchUseless = findViewById(R.id.switch_main_useless);
+        }
+    }
+
